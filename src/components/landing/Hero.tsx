b871637ee31f5952547bc9a142f48b4156e63234
kingz75 <kingsufpay@gmail.com>
2026-02-18 14:43:50 +0100

    fix

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -98,7 +98,7 @@ export function Hero() {
       </motion.div>
 
       {/* Main Content */}
-      <div className="relative z-10 max-w-7xl mx-auto px-6 lg:px-8 text-center  ">
+      <div className="relative z-10 max-w-7xl mx-auto px-6 lg:px-8 text-center mt-10  ">
         {/* Badge */}
         {/* <motion.div
           initial={{ opacity: 0, y: 20 }}
